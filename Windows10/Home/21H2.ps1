1e2b2c58bc6f49fdf0e050ba5284a33df7ef69b0
danijeljw-rpc <danijel@repasscloud.com>
2022-05-05 15:49:38 +1000

    revised code

--- Windows10/Home/21H2.ps1
+++ Windows10/Home/21H2.ps1
@@ -17,10 +17,10 @@ $CHeaders = @{accept = 'application/json'}
 
 <# CONFIG #>
 [System.String]$WinRelease = "10"
-[System.String]$WinEdition = "Pro"  # WindowsEdition (Home, Pro, Pro_N, Education, Education_N, Enterprise, Enterprise_N, Pro_Education, Pro_Education_N, Pro_Workstations,Pro_N_Workstations, Enterprise_LTSC)
+[System.String]$WinEdition = "Home"  # WindowsEdition (Home, Pro, Pro_N, Education, Education_N, Enterprise, Enterprise_N, Pro_Education, Pro_Education_N, Pro_Workstations,Pro_N_Workstations, Enterprise_LTSC)
 [System.String]$WinArch = "x64"
-[System.String]$FidoRelease = "21H1"  # WindowsVersion (v21H2, v21H1, v20H2, v2004, v1909, v1903, v1809, v1809, v1803, v1709, v1703, v1607, v1511, v1507)
-[System.String]$WinLcid = "Estonian"
+[System.String]$FidoRelease = "21H2"  # WindowsVersion (v21H2, v21H1, v20H2, v2004, v1909, v1903, v1809, v1809, v1803, v1709, v1703, v1607, v1511, v1507)
+[System.String]$WinLcid = "English"
 [System.String]$SupportedWinRelease = "Windows ${WinRelease}"  # WindowsRelease (Windows_7, Windows_8, Windows_8_1, Windows_10, Windows_11) <~ see repasscloud/WindowsCapability/issues/2
 
 <# SETUP #>
@@ -79,7 +79,7 @@ Get-WindowsCapability -Path "${env:TMP}\Win${WinRelease}_${FidoRelease}_${WinLci
                 supportedWindowsReleases = @($RecordFound.supportedWindowsReleases)
             } | ConvertTo-Json
             Write-Output "<| Test SupportedWindowsVersions"
-            Invoke-RestMethod -Uri "${env:API_URI}/v1/WindowsCapability/${Id}" -Method Put -Body $Body -ContentType 'application/json' -ErrorAction Stop
+            Invoke-RestMethod -Uri "${env:API_URI}/v1/WindowsCapability/${Id}" -Method Put -UseBasicParsing -Body $Body -ContentType 'application/json' -ErrorAction Stop
         }
         else
         {
