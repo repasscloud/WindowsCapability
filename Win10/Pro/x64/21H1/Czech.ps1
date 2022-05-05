bd22b16c74cc2f914916c62204429791fce7ba43
danijeljw-rpc <danijel@repasscloud.com>
2022-05-05 12:31:55 +1000

    introduce new languages to 21H1

--- Win10/Pro/x64/21H1/Czech.ps1
+++ Win10/Pro/x64/21H1/Czech.ps1
@@ -20,7 +20,7 @@ $CHeaders = @{accept = 'application/json'}
 [System.String]$WinEdition = "Pro"  # WindowsEdition (Home, Pro, Pro_N, Education, Education_N, Enterprise, Enterprise_N, Pro_Education, Pro_Education_N, Pro_Workstations,Pro_N_Workstations, Enterprise_LTSC)
 [System.String]$WinArch = "x64"
 [System.String]$FidoRelease = "21H1"  # WindowsVersion (v21H2, v21H1, v20H2, v2004, v1909, v1903, v1809, v1809, v1803, v1709, v1703, v1607, v1511, v1507)
-[System.String]$WinLcid = "English"
+[System.String]$WinLcid = "Czech"
 [System.String]$SupportedWinRelease = "Windows ${WinRelease}"  # WindowsRelease (Windows_7, Windows_8, Windows_8_1, Windows_10, Windows_11) <~ see repasscloud/WindowsCapability/issues/2
 
 <# SETUP #>
