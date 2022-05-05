d638d0777c01d1c403849092530885e0fe76a0e2
danijeljw-rpc <danijel@repasscloud.com>
2022-05-05 10:46:20 +1000

    update script to fix #2

--- Win10/Pro/x64/21H2/English.ps1
+++ Win10/Pro/x64/21H2/English.ps1
@@ -21,7 +21,7 @@ $CHeaders = @{accept = 'application/json'}
 [System.String]$WinArch = "x64"
 [System.String]$FidoRelease = "21H2"  # WindowsVersion (v21H2, v21H1, v20H2, v2004, v1909, v1903, v1809, v1809, v1803, v1709, v1703, v1607, v1511, v1507)
 [System.String]$WinLcid = "English"
-[System.String]$SupportedWinRelease = "Windows_10"  # WindowsRelease (Windows_7, Windows_8, Windows_8_1, Windows_10, Windows_11)
+[System.String]$SupportedWinRelease = "Windows ${WinRelease}"  # WindowsRelease (Windows_7, Windows_8, Windows_8_1, Windows_10, Windows_11) <~ see repasscloud/WindowsCapability/issues/2
 
 <# SETUP #>
 [System.String]$DownloadLink = & $FidoFile -Win $WinRelease -Rel $FidoRelease -Ed $WinEdition -Lang $WinLcid -Arch $WinArch -GetUrl
@@ -137,13 +137,12 @@ Get-WindowsCapability -Path "${env:TMP}\Win${WinRelease}_${FidoRelease}_${WinLci
     }
     catch
     {
-        $SupportedWindowsRelease = "v" + $FidoRelease
         $Body = @{
             id = 0
             uuid = [System.Guid]::NewGuid().Guid.ToString()
             name = $Name
             present = $Enabled
-            supportedWindowsVersions = @($SupportedWindowsRelease)
+            supportedWindowsVersions = @($FidoRelease)
             supportedWindowsEditions = @($WinEdition)
             supportedWindowsReleases = @($SupportedWinRelease)
         } | ConvertTo-Json
